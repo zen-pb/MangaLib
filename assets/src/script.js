df4a72c22eb140ed05386fc7792bd9829d61cb1a
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-07-31 13:29:06 +0800

    Add image handler, err handler, display function

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,21 +1,66 @@
+const container = document.querySelector(".container");
+
+const myLibrary = [
+  {
+    img: "../assets/images/jjk-vol25.svg",
+    title: "Jujutsu Kaisen VOL. 25",
+    author: "Gege Akutami",
+    pages: "218",
+    status: "read",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
+  displayLibrary();
+
   const addBookButton = document.getElementById("addBookButton");
   const modalAddBook = document.getElementById("modalAddBook");
   const modalCloseButton = document.getElementById("modalCloseButton");
 
   const addBookForm = document.getElementById("addBookForm");
+  const exists = document.getElementById("alreadyExists");
+  const submitBookButton = document.getElementById("submitBook");
+
+  const titleInput = document.getElementById("title");
+  let title = "";
+
+  titleInput.addEventListener("input", (event) => {
+    title = event.target.value;
+
+    console.log(title);
+
+    if (bookExists(title)) {
+      exists.classList.add("exists");
+      submitBookButton.disabled = true;
+    } else {
+      exists.classList.remove("exists");
+      submitBookButton.disabled = false;
+    }
+  });
 
   addBookForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const title = document.getElementById("title").value;
+    const uploadedImage = document.getElementById("uploadedImage");
+    const image = uploadedImage.files[0];
+    let imageURL = "";
+
+    if (image) {
+      imageURL = URL.createObjectURL(image);
+    } else {
+      imageURL = "../assets/images/default.svg";
+    }
+
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
     const status = document.querySelector('input[name="status"]:checked').value;
 
-    addBookToLibrary(Book(title, author, pages, status));
-
-    closeModal();
+    if (!bookExists(title)) {
+      addBookToLibrary(Book(imageURL, title, author, pages, status));
+      closeModal();
+      flush();
+      displayLibrary();
+    }
   });
 
   addBookButton.addEventListener("click", () => {
@@ -23,16 +68,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   modalCloseButton.addEventListener("click", closeModal);
+
+  function closeModal() {
+    modalAddBook.classList.remove("open");
+  }
 });
 
-function closeModal() {
-  modalAddBook.classList.remove("open");
-}
-
-const myLibrary = [];
-
-function Book(title, author, pages, status) {
+function Book(imageURL, title, author, pages, status) {
   return {
+    img: imageURL,
     title: title,
     author: author,
     pages: pages,
@@ -44,13 +88,85 @@ function addBookToLibrary(bookObject) {
   myLibrary.push(bookObject);
 }
 
-function createCard() {
-  const card = document.createElement("div");
-  card.classList.add("card");
+function displayLibrary() {
+  myLibrary.forEach((book) => {
+    if (cardChecker(book.title)) {
+      return;
+    } else {
+      const card = document.createElement("div");
+      const bookDiv = document.createElement("div");
+      const bookImg = document.createElement("img");
+      const bookTitle = document.createElement("p");
+      const bookAuthor = document.createElement("p");
+      const bookPages = document.createElement("p");
+      const label = document.createElement("label");
+      const bookStatus = document.createElement("input");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
 
-  const cardAuthor = document.createElement("p");
-  cardAuthor.classList.add("author");
+      card.classList.add("card");
+      bookDiv.classList.add("book");
+      bookTitle.classList.add("title");
+      bookAuthor.classList.add("author");
+      bookPages.classList.add("pages");
+      label.classList.add("switch");
+      bookStatus.type = "checkbox";
+      span.classList.add("slider");
 
-  const cardPages = document.createElement("p");
-  cardPages.classList.add("pages");
+      bookImg.src = book.img;
+      bookTitle.textContent = book.title;
+      bookAuthor.textContent = book.author;
+      bookPages.textContent = `${book.pages} pages`;
+      bookStatus.checked = book.status === "read";
+      p.textContent = "Read";
+
+      bookDiv.appendChild(bookImg);
+      bookDiv.appendChild(bookTitle);
+      bookDiv.appendChild(bookAuthor);
+      bookDiv.appendChild(bookPages);
+
+      label.appendChild(bookStatus);
+      span.appendChild(p);
+      label.appendChild(span);
+
+      card.appendChild(bookDiv);
+      card.appendChild(label);
+
+      container.appendChild(card);
+    }
+  });
+}
+
+function bookExists(bookTitle) {
+  let exists = false;
+  let title = bookTitle.toLowerCase().replace(/ /g, "");
+
+  myLibrary.forEach((book) => {
+    let bookLib = book.title.toLowerCase().replace(/ /g, "");
+
+    if (bookLib === title) {
+      exists = true;
+    }
+  });
+
+  return exists;
+}
+
+function cardChecker(bookTitle) {
+  let exists = false;
+  const book = document.querySelector(".book");
+
+  if (book != null && book.innerHTML.includes(bookTitle)) {
+    exists = true;
+  }
+
+  return exists;
+}
+
+function flush() {
+  imageURL = "";
+  title.textContent = "";
+  author.textContent = "";
+  pages.textContent = "";
+  status.textContent = "";
 }
